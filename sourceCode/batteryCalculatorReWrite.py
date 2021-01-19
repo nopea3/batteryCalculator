9955fb01eca2f2a4e910fb8b867fb8fb47f95b72
nopea3 <61728220+nopea3@users.noreply.github.com>
2021-01-19 22:33:27 +0200

    Update batteryCalculatorReWrite.py

--- sourceCode/batteryCalculatorReWrite.py
+++ sourceCode/batteryCalculatorReWrite.py
@@ -1,8 +1,8 @@
 import time
 import requests
 
-response = requests.get('https://www.nkon.nl/lg-inr18650-mj1.html')
-print(response.json)
+#response = requests.get('https://www.nkon.nl/lg-inr18650-mj1.html')
+#print(response.)
 
 def askForCellType():
     name = input('Minkä kennon haluat ')
